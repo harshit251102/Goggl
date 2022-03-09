94b67e3a82275738172ffedba66053b9150bc907
harshit251102 <87938754+harshit251102@users.noreply.github.com>
2022-03-09 10:10:05 +0530

    Initial Commmit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {ResultContextProvider} from './context/ResultContextProvider';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ResultContextProvider>
+      <Router>
+        <App />
+      </Router>
+  </ResultContextProvider>,
   document.getElementById('root')
 );
 
